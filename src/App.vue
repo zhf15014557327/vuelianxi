269be447a9d40a1b571c9b60f7fbe676f4352f5e
Your Name <118735992@.con>
2019-03-18 22:49:50 +0800

    创建组件!安装sass编写组件样式

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 组件 -->
+    <div class="chajian">
+      <input type="button" value="+" @click="jia">
+      <span>{{initnum}}</span>
+      <input type="button" value="-" @click="jian">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  data() {
+    return {
+      initnum: 15,
+      initmax: 30,
+      initmin: 1,
+      initbushu: 2
+    };
+  },
+  methods: {
+    jia() {
+      this.initnum += this.initbushu;
+      if (this.initnum > this.initmax) {
+        this.initnum = this.initmax;
+      }
+    },
+    jian() {
+      this.initnum -= this.initbushu;
+      if (this.initnum < this.initmin) {
+        this.initnum = this.initmin;
+      }
+    }
+  },
+  components: {}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.chajian {
+  margin: 300px auto;
+  width: 150px;
+  height: 100%;
+  input {
+    width: 50px;
+    height: 100%;
+  }
+  span {
+    width: 50px;
+    height: 100%;
+    text-align: center;
+    display: inline-block;
+  }
 }
 </style>
